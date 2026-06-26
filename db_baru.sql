104e5cc36c414af681960de502a531f8c5a75da4
merlls11 <mariananadyaputri@gmail.com>
2026-06-26 08:12:13 +0700

    Add files via upload

---
+++ db_baru.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2026 at 03:11 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_baru`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` varchar(10) NOT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `satuan` varchar(20) NOT NULL,
+  `harga_jual` double NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `id_kategori`, `nama_barang`, `satuan`, `harga_jual`, `stok`) VALUES
+('BRG001', 1, 'Ciki Taro', 'pcs', 3000, 166),
+('BRG002', 2, 'Kopi Susu', 'pcs', 23000, 10),
+('BRG003', 1, 'Sabun mandi', 'pcs', 5000, 50),
+('BRG004', 2, 'Coca cola', 'pcs', 7500, 49),
+('BRG005', 4, 'Tumbler', 'pcs', 45000, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_customer`
+--
+
+CREATE TABLE `tb_customer` (
+  `id_customer` varchar(10) NOT NULL,
+  `nama_customer` varchar(100) NOT NULL,
+  `alamat` text DEFAULT NULL,
+  `telepon` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_customer`
+--
+
+INSERT INTO `tb_customer` (`id_customer`, `nama_customer`, `alamat`, `telepon`) VALUES
+('CST001', 'mariana', 'jl panjang', '0123456'),
+('CST002', 'nunu', 'jl kebun', '12345'),
+('CST004', 'riri', 'jl kena', '1234'),
+('CST005', 'mari', 'jl melati', '123456'),
+('CST006', 'iraa', 'jl putih', '123456'),
+('CST007', 'rasysiq', 'jl bogor', '01234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail_penjualan`
+--
+
+CREATE TABLE `tb_detail_penjualan` (
+  `id_detail` int(11) NOT NULL,
+  `id_jual` int(11) DEFAULT NULL,
+  `id_barang` varchar(10) DEFAULT NULL,
+  `harga_satuan` double NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `subtotal` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_detail_penjualan`
+--
+
+INSERT INTO `tb_detail_penjualan` (`id_detail`, `id_jual`, `id_barang`, `harga_satuan`, `jumlah_beli`, `subtotal`) VALUES
+(4, 6, 'BRG001', 3000, 1, 3000),
+(5, 6, 'BRG002', 23000, 2, 46000),
+(6, 7, 'BRG001', 3000, 3, 9000),
+(7, 7, 'BRG002', 23000, 1, 23000),
+(8, 8, 'BRG001', 3000, 2, 6000),
+(10, 10, 'BRG001', 3000, 2, 6000),
+(11, 10, 'BRG002', 23000, 2, 46000),
+(15, 14, 'BRG002', 23000, 44, 1012000),
+(19, 17, 'BRG002', 23000, 50, 1150000),
+(20, 18, 'BRG004', 7500, 3, 22500),
+(21, 18, 'BRG001', 3000, 2, 6000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'MAKANAN RINGAN'),
+(2, 'MINUMAN'),
+(3, 'PERLENGKAPAN KEBERSIHAN'),
+(4, 'PERLENGKAPAN PRIBADI');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `id_barang` varchar(20) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penjualan`
+--
+
+CREATE TABLE `tb_penjualan` (
+  `id_jual` int(11) NOT NULL,
+  `no_faktur` varchar(20) DEFAULT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `id_customer` varchar(10) DEFAULT NULL,
+  `total_bayar` double NOT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_penjualan`
+--
+
+INSERT INTO `tb_penjualan` (`id_jual`, `no_faktur`, `tgl_transaksi`, `id_customer`, `total_bayar`, `id_user`) VALUES
+(6, 'FAK-001', '2026-06-12', 'CST001', 49000, NULL),
+(7, 'FAK-002', '2026-06-12', 'CST002', 32000, NULL),
+(8, 'FAK-003', '2026-06-12', 'CST002', 6000, NULL),
+(10, 'FAK-005', '2026-06-16', 'CST001', 52000, NULL),
+(14, 'FAK-009', '2026-06-17', NULL, 1012000, NULL),
+(17, 'FAK-010', '2026-06-18', 'CST005', 1150000, NULL),
+(18, 'FAK-011', '2026-06-19', 'CST007', 28500, NULL);
+
+--
+-- Triggers `tb_penjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_penjualan` BEFORE INSERT ON `tb_penjualan` FOR EACH ROW BEGIN
+    DECLARE next_id INT;
+    SET next_id = (SELECT IFNULL(MAX(CAST(SUBSTRING(no_faktur, 5) AS UNSIGNED)), 0) + 1 FROM tb_penjualan);
+    SET NEW.no_faktur = CONCAT('FAK-', LPAD(next_id, 3, '0'));
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `level` enum('Admin','Kasir','Customer') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `username`, `password`, `nama_lengkap`, `level`) VALUES
+(1, 'admin', 'admin', 'mei admin', 'Admin'),
+(2, 'mariana', 'kasir', 'mariana nadya', 'Kasir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `tb_customer`
+--
+ALTER TABLE `tb_customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indexes for table `tb_detail_penjualan`
+--
+ALTER TABLE `tb_detail_penjualan`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_jual` (`id_jual`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_keranjang`);
+
+--
+-- Indexes for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  ADD PRIMARY KEY (`id_jual`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_detail_penjualan`
+--
+ALTER TABLE `tb_detail_penjualan`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  MODIFY `id_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD CONSTRAINT `tb_barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_detail_penjualan`
+--
+ALTER TABLE `tb_detail_penjualan`
+  ADD CONSTRAINT `tb_detail_penjualan_ibfk_1` FOREIGN KEY (`id_jual`) REFERENCES `tb_penjualan` (`id_jual`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_detail_penjualan_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  ADD CONSTRAINT `tb_penjualan_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `tb_customer` (`id_customer`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_penjualan_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
